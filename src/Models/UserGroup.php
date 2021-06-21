4dfa5fdfd1bc8c886c0489cd103e9d3d03ba5851
mishu <mishu@xtelecom.ro>
2021-06-21 10:17:19 +0300

    adds missing import

--- src/Models/UserGroup.php
+++ src/Models/UserGroup.php
@@ -2,6 +2,7 @@
 
 namespace LaravelEnso\UserGroups\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
@@ -39,7 +40,7 @@ class UserGroup extends Model
         parent::delete();
     }
 
-    public function scopeVisible($query)
+    public function scopeVisible(Builder $query): Builder
     {
         $isSuperior = Auth::user()->belongsToAdminGroup();
 
